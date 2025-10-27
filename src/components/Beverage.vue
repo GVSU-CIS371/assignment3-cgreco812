ccf9a55b407fa2414dd6ec16182e8067a9ce1209
Charlie Greco <cgreco812@gmail.com>
2025-10-27 19:59:40 -0400

    Added code from assignment 2

--- src/components/Beverage.vue
+++ src/components/Beverage.vue
@@ -3,14 +3,14 @@
     <Cold v-if="isIced" />
     <Hot v-else />
     <Contents>
-      <template v-slot:top>
-        <Creamer />
+      <template v-slot:top v-if="creamer.id !='c1'">
+        <Creamer :creamer="creamer"/>
       </template>
-      <template v-slot:mid>
-        <Syrup />
+      <template v-slot:mid v-if="syrup.id !='s1'">
+        <Syrup :syrup="syrup"/>
       </template>
       <template v-slot:bottom>
-        <Base />
+        <Base :base="base"/>
       </template>
     </Contents>
   </Mug>
@@ -23,8 +23,12 @@ import Base from "./Base.vue";
 import Creamer from "./Creamer.vue";
 import Hot from "./Hot.vue";
 import Cold from "./Cold.vue";
+
 type Props = {
   isIced: boolean;
+  base: any;
+  creamer: any;
+  syrup: any
 };
 defineProps<Props>();
 </script>
